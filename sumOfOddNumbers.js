e53c6db07cf1df0fde5092b51335a4cc744d98d7
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-12-05 07:46:34 +0200

    11. Sum of Odd Numbers
    update with better logic

--- sumOfOddNumbers.js
+++ sumOfOddNumbers.js
@@ -1,11 +1,11 @@
-function solve(n){
-  let oddNum = 1;
-  let allSum = 0;
-  for (i = n; n >= 1; n-- ){
-    console.log(oddNum);
-    allSum += oddNum;
-     oddNum += 2;
-  }
-  console.log(`Sum: ${allSum}`);
+function solve(num) {
+    let oddSum = 1;
+    let maxSum = 0;
+    for (let i = 1; i <= num; i++) {
+        console.log(oddSum);
+        maxSum += oddSum;
+        oddSum += 2;
+    }
+    console.log(`Sum: ${maxSum}`);
 }
-solve(5)
+solve(5);
