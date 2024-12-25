4f472bd3639fac5fdaadce768276a999d0048510
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2024-12-25 23:23:07 +0200

    07. Triangle Of Numbers

---
+++ triangleOfNumbers.js
@@ -0,0 +1,10 @@
+function solve(num) {
+    for (let i = 1; i <= num; i++) {
+        let line = '';
+        for (let j = 1; j <= i; j++) {
+            line += i + ' ';
+        }
+        console.log(line.trim());
+    }
+}
+solve(6)
