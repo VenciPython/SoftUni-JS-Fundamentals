4b5d1a09eb75ae88ffd4bd803e99f17a2c2dc73f
Ventsislav Zahariev <91217329+VenciPython@users.noreply.github.com>
2025-01-25 11:07:54 +0200

    04. Reverse in Place
    array reverse method

---
+++ reverseInPlace.js
@@ -0,0 +1,7 @@
+function solve(arr) {
+    let result = arr.reverse()
+    console.log(result.join(' '));
+
+}
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
